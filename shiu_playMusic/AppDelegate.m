bc6832414be7364fa9c2506d98c4c3d4a79b6b4a
allen_hsu <allen_hsu@administratordeMacBook-Air-7.local>
2015-10-19 21:35:13 +0800

    1.資源檔案資料夾分類。 2.整理 AppDelegate。 3.整理 DownloadMusicViewController。

--- shiu_playMusic/AppDelegate.m
+++ shiu_playMusic/AppDelegate.m
@@ -9,41 +9,30 @@
 #import "AppDelegate.h"
 #import "VideoListViewController.h"
 #import "DownloadMusicViewController.h"
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
-    UINavigationController *musicsListNavigationController = [[UINavigationController alloc] init];
-    VideoListViewController *musicsListView = [[VideoListViewController alloc] init];
-    [musicsListNavigationController pushViewController:musicsListView animated:NO];
-    UITabBarItem *musicsListBarItem=[[UITabBarItem alloc]initWithTitle:@"Audio" image:[UIImage imageNamed:@"musicVideoIcon.png"] selectedImage:[UIImage imageNamed:@"musicVideoIcon.png.png"]];
-    musicsListNavigationController.tabBarItem=musicsListBarItem;
+    UINavigationController *musicsListNavigationController = [[UINavigationController alloc] initWithRootViewController:[VideoListViewController new]];
+    UITabBarItem *musicsListBarItem = [[UITabBarItem alloc] initWithTitle:@"Audio" image:[UIImage imageNamed:@"musicVideoIcon.png"] selectedImage:[UIImage imageNamed:@"musicVideoIcon.png"]];
+    musicsListNavigationController.tabBarItem = musicsListBarItem;
 
-    UINavigationController *downloadMusicNavigationController = [[UINavigationController alloc] init];
-    DownloadMusicViewController *downloadMusicView = [[DownloadMusicViewController alloc] init];
-    [downloadMusicNavigationController pushViewController:downloadMusicView animated:NO];
-    UITabBarItem *downloadBarItem=[[UITabBarItem alloc]initWithTitle:@"Download" image:[UIImage imageNamed:@"downloadIcon.png"] selectedImage:[UIImage imageNamed:@"downloadIcon.png"]];
-    downloadMusicNavigationController.tabBarItem=downloadBarItem;
-    
+    UINavigationController *downloadMusicNavigationController = [[UINavigationController alloc] initWithRootViewController:[DownloadMusicViewController new]];
+    UITabBarItem *downloadBarItem = [[UITabBarItem alloc] initWithTitle:@"Download" image:[UIImage imageNamed:@"downloadIcon.png"] selectedImage:[UIImage imageNamed:@"downloadIcon.png"]];
+    downloadMusicNavigationController.tabBarItem = downloadBarItem;
+
     //  宣告 TabBarController
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
     //  將物件加入 TabBarController
-    tabBarController.viewControllers = [NSArray arrayWithObjects: downloadMusicNavigationController,musicsListNavigationController , nil];
-
+    tabBarController.viewControllers = @[downloadMusicNavigationController, musicsListNavigationController];
     self.window.rootViewController = tabBarController;
     [self.window makeKeyAndVisible];
-
-
-
-
-
-
     return YES;
 }
 
