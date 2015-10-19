d01352661d40779d55dbae3f74169f39ccaddca1
allen_hsu <allen_hsu@administratordeMacBook-Air-7.local>
2015-10-19 18:16:00 +0800

    no message

--- shiu_playMusic/MusicsListViewController/VideoListViewController.m
+++ shiu_playMusic/MusicsListViewController/VideoListViewController.m
@@ -13,32 +13,35 @@
 @interface VideoListViewController ()
 {
 
-    BOOL isPlayingAideo;
-
+    BOOL isPlayingVideos;
+    int currentValue;
 }
-@property (weak, nonatomic) IBOutlet PlayVideoView *playVideoView;
-@property (weak, nonatomic) IBOutlet UITableView *videoInfoTableView;
-@property (weak, nonatomic) IBOutlet UIButton *playVideoButton;
-@property (weak, nonatomic) IBOutlet UIButton *videoRateButton;
-@property (weak, nonatomic) IBOutlet UIButton *fullScreenButton;
-@property (weak, nonatomic) IBOutlet UILabel *currentlyTimeLabel;
-@property (weak, nonatomic) IBOutlet UILabel *totalTimeLabel;
-@property (weak, nonatomic) IBOutlet UIView *controlButtonView;
-@property (weak, nonatomic) IBOutlet UISlider *currentVideoSlider;
+@property (nonatomic, weak) IBOutlet PlayVideoView *playVideoView;
+@property (nonatomic, weak) IBOutlet UITableView *videoInfoTableView;
+@property (nonatomic, weak) IBOutlet UIButton *playVideoButton;
+@property (nonatomic, weak) IBOutlet UIButton *videoRateButton;
+@property (nonatomic, weak) IBOutlet UIButton *fullScreenButton;
+@property (nonatomic, weak) IBOutlet UILabel *currentlyTimeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *totalTimeLabel;
+@property (nonatomic, weak) IBOutlet UIView *controlButtonView;
+@property (nonatomic, weak) IBOutlet UISlider *currentVideoSlider;
+@property(nonatomic, strong) NSArray *videos;
 @end
 
 @implementation VideoListViewController
+
 #pragma mark - GestureAction
 
 - (IBAction)tapGestureRecognizerAction:(id)sender {
     self.controlButtonView.hidden = !self.controlButtonView.hidden;
     self.playVideoButton.hidden = !self.playVideoButton.hidden;
     self.navigationController.navigationBar.hidden = !self.navigationController.navigationBar.hidden;
+
 }
 
 #pragma mark - IBAction
 - (IBAction)currentVideoSliderAction:(id)sender {
-    CMTime newTime = CMTimeMakeWithSeconds(self.currentVideoSlider.value,self.currentVideoSlider.maximumValue);
+    CMTime newTime = CMTimeMakeWithSeconds(self.currentVideoSlider.value, self.currentVideoSlider.maximumValue);
     [self.playVideoView.player seekToTime:newTime];
 
 }
@@ -46,18 +49,18 @@
 - (IBAction)videoRateButtonAction:(id)sender {
 
     [self.playVideoView.player setRate:2.0];
-   
+
 }
 - (IBAction)playMusicButtonAction:(id)sender {
-    if (isPlayingAideo) {
+    if (isPlayingVideos) {
         [self.playVideoButton setTitle:@"play" forState:UIControlStateNormal];
         [self.playVideoView.player pause];
-        isPlayingAideo = NO;
+        isPlayingVideos = NO;
     }
     else {
         [self.playVideoButton setTitle:@"pause" forState:UIControlStateNormal];
         [self.playVideoView.player play];
-        isPlayingAideo = YES;
+        isPlayingVideos = YES;
     }
 
 }
@@ -79,30 +82,35 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-
-    [self.playVideoView.player play];
+    currentValue=(int)indexPath.row;
+    [self playVideoFuntion];
+    
 }
 - (void)replaceCurrentItemWithPlayerItem:(nullable AVPlayerItem *)item {
 }
 #pragma mark - TableView DataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return self.videos.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *identifier = @"VideoListViewCell";
     VideoListViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
-    cell.videoImageView.image = [self getTheMovieImage:@"like"];
+    cell.videoImageView.image = [self getTheMovieImage:[self.videos objectAtIndex:indexPath.row]];
     return cell;
 }
 
 #pragma mark - private instance method
-- (void)playVideoFuntion:(NSString *)strFileName {
-    isPlayingAideo = NO;
+- (void)playVideoFuntion {
+    isPlayingVideos = NO;
+
+
+
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsPath = [NSString stringWithFormat:@"%@/%@", [paths objectAtIndex:0], paths];
-    NSString *path = [[NSBundle mainBundle] pathForResource:strFileName ofType:@"m4v"];
+
+    NSString *path = [[NSBundle mainBundle] pathForResource:[self.videos objectAtIndex:currentValue] ofType:@"m4v"];
 
     NSURL *fileURL = [NSURL fileURLWithPath:path];
     AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:fileURL];
@@ -114,14 +122,14 @@
     self.totalTimeLabel.text = [self formatTime:TotalSeconds];
     //設定currentVideoSlider 的長度
     [self.currentVideoSlider setMaximumValue:TotalSeconds];
-    
+
     [self.playVideoView.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 2) queue:dispatch_get_main_queue() usingBlock: ^(CMTime time) {
          int currentTime = CMTimeGetSeconds(self.playVideoView.player.currentTime);
          [self.currentVideoSlider setValue:currentTime];
          self.currentlyTimeLabel.text = [self formatTime:currentTime];
      }];
 
-
+    [self.playVideoView.player play];
 }
 //秒轉分鐘
 - (NSString *)formatTime:(int)num {
@@ -134,7 +142,7 @@
     AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:fileName ofType:@"m4v"]] options:nil];
     AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
     //選擇要擷取圖片的時間範圍
-    CMTime time = CMTimeMake(2, 2); // time range in which you want
+    CMTime time = CMTimeMake(10,10); // time range in which you want
     CGImageRef imgRef = [imageGenerator copyCGImageAtTime:time actualTime:NULL
 
                                                     error:nil];
@@ -143,11 +151,13 @@
 }
 //下一首
 - (void)nextVideoButtonAction:(id)sender {
-
+    currentValue++;
+    [self playVideoFuntion];
 }
 //上一首
 - (void)periousVideoButtonAction:(id)sender {
-
+    currentValue--;
+    [self playVideoFuntion];
 }
 #pragma mark - life cycle
 - (void)viewDidLoad {
@@ -170,8 +180,10 @@
                                                                   target:self action:@selector(periousVideoButtonAction:)];
 
 
-    [self playVideoFuntion:@"like"];
-    
+   // [self playVideoFuntion:[self.videos objectAtIndex:1]];
+    self.videos = @[@"like", @"Movie",@"我能給的"];
+    currentValue=0;
+    [self playVideoFuntion];
 
 }
 
